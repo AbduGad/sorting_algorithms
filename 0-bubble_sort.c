6b0b18b20d43b1d4138ad6658fe6b847db523a90
AbduGad <abdo79gad@gmail.com>
2023-10-22 10:58:02 +0300

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorting with bubble sort type
+ *
+ * @array: array to sort
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int swap_flag = 1;
+	int tmp;
+	size_t i;
+
+	while (swap_flag)
+	{
+		swap_flag = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				swap_flag = 1;
+				print_array(array, size);
+			}
+		}
+	}
+	
+}
